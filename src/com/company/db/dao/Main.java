80b9bfd7bf901093c358ea6287a5ba8fbc961ac4
ea-vorobyev <vorobyevea88@gmail.com>
2017-10-13 11:00:22 +0300

    1.2

--- src/com/company/db/dao/Main.java
+++ src/com/company/db/dao/Main.java
@@ -21,9 +21,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        System.out.println("!");
         //deleteDB();
         //getFromDB();
-        getFromXML();
+        //getFromXML();
     }
 
 }
